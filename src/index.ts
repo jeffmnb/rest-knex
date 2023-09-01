e3214d677ec0e6d21aef39a3a5a7813f82c074af
Jeferson <jeferson.nborda@gmail.com>
2023-09-01 18:45:04 -0300

    chore: update lint

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import express from "express";
 import { router as produtosRouter } from "./routes/produtos.routes";
 
 const app = express();
-
 app.use(express.json());
+
 app.use("/produtos", produtosRouter);
 
 app.listen(3000, () => {
