d80e2979db813d804b21cdf0a1a3c995cc8de616
Jeferson Borda <70456966+jeffmnb@users.noreply.github.com>
2023-09-04 21:05:55 -0300

    update seeds

--- db/seeds/carga-produto.js.js
+++ db/seeds/carga-produto.js.js
@@ -9,5 +9,6 @@ exports.seed = async function (knex) {
     { id: 1, descricao: "Macbook Air", marca: "Apple", preco: 6.999 },
     { id: 2, descricao: "Macbook Pro", marca: "Apple", preco: 8.999 },
     { id: 3, descricao: "Iphone 14 Pro Max", marca: "Apple", preco: 7.999 },
+    { id: 4, descricao: "Watch Ultra", marca: "Apple", preco: 6.999 },
   ]);
 };
